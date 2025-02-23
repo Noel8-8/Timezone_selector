5371896aa77150b01a9dc7ea25a842046ed30b01
Noel8-8 <noelmoore100@gmail.com>
2025-02-23 16:58:04 +0700

    finishing styling and adding the scripting to the project

--- src/main.js
+++ src/main.js
@@ -14,16 +14,16 @@ dayjs.extend(utc);
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const timezoneElement = document.querySelector('.timezone');
+let timezoneShown = dayjs.tz.guess()
 
-timezoneElement.innerHTML = dayjs.tz.guess();
+timezoneElement.innerHTML = timezoneShown;
 time.innerHTML = now.format('HH:mm:ss');
 date.innerHTML = `${now.format('dddd, D MMMM, YYYY')}`;
 
 
-
 setInterval(() => {
-  now = dayjs();
-  timezoneElement.innerHTML = dayjs.tz.guess();
+  now = dayjs().tz(timezoneShown)
+  timezoneElement.innerHTML = timezoneShown;
   time.innerHTML = now.format('HH:mm:ss');
   date.innerHTML = `${now.format('dddd, D MMMM, YYYY')}`;
 }, 1000);
@@ -32,13 +32,64 @@ setInterval(() => {
 const timezones = moment.tz.names();
 const modalMenu = document.querySelector('.modalMenu');
 
-const html = timezones.map((timezone) => {
+let html = timezones.map((timezone) => {
   return`
   <div class="item" data-micromodal-close>
-    <h2 class="timezone">${timezone}</h2>
+    <h2 class="timezoneModal">${timezone}</h2>
     <p>${now.tz(`${timezone}`).format('HH:mm:ss')}</p>
   </div>
   `
 }).join('');
 
 modalMenu.innerHTML = html;
+
+
+// sorting modal animations
+function handleTimezoneClick(item){
+  const timezoneBold = item.querySelector('.timezoneModal').innerHTML;
+  const timezone = timezoneBold.replace(/<span[^>]*>(.*?)<\/span>/gi, "$1");
+
+  timezoneShown = timezone;
+}
+
+modalMenu.addEventListener('click', (e)=>{
+  if(e.target.closest('.item')){
+    const item = e.target.closest('.item');
+    handleTimezoneClick(item)
+  }
+})
+
+const searchBox = document.querySelector('.searchBox');
+
+function handleSearch(){
+  if(this.value){
+    html = timezones.filter((timezone) => {
+      if(timezone.toLowerCase().includes(searchBox.value.toLowerCase())){
+        return timezone;
+      }
+    })
+    .map((timezone)=>{
+      const highlight = this.value
+      let regex = new RegExp(highlight, "gi");
+      const boldedTimezone = timezone.replace(regex,
+         `<span style="font-weight: 600;" data-micromodal-close>$&</span>`
+      )
+
+      return `
+      <div class="item" data-micromodal-close>
+        <h2 class="timezoneModal">${boldedTimezone}</h2>
+        <p>${now.tz(`${timezone}`).format('HH:mm:ss')}</p>
+      </div>
+      `
+    }).join('')
+    modalMenu.innerHTML = html;
+    modalMenu.classList.remove('hidden')
+    
+  }else{
+    modalMenu.classList.add('hidden')
+  }
+  
+}
+
+searchBox.addEventListener('input', handleSearch)
+
